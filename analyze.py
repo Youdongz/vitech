017b4f5721c693ebd9b36ab55082b15634a01949
youdongz <youdongz@berkeley.edu>
2017-12-02 18:12:11 -0500

    boiler and new visualization code

--- analyze.py
+++ analyze.py
@@ -1,6 +1,6 @@
 import json
 
-BRONZE = 'BRONZE'
+BRONZE = "BRONZE"
 SILVER = 'SILVER'
 GOLD = 'GOLD'
 PLATINUM = 'PLATINUM'
@@ -9,9 +9,14 @@ classes = {}
 with open('decisions_10000.json', 'r') as f:
 	data = json.load(f)
 	for doc in data:
-		key = doc[BRONZE] + " " + doc[SILVER] + doc[GOLD] + doc[PLATINUM]
+		key = (doc[BRONZE] if BRONZE in doc else 0, doc[SILVER], doc[GOLD], doc[PLATINUM])
 		if key not in classes:
 			classes[key] = 0
 
 		classes[key] += 1
-	print classes
+
+lst = [[key] + [classes[key]] for key in classes]
+sort = sorted(lst, key=lambda k: k[0])
+values = [each[1] for each in sort]
+print sort
+print values
