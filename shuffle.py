44e1ee6a337c8dc296ef742209daab6b405c2e38
youdongz <youdongz@berkeley.edu>
2017-12-02 09:57:17 -0500

    complete? basic data

--- shuffle.py
+++ shuffle.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
-with open('data.json', 'r') as f:
+with open('basic.json', 'r') as f:
+	data = f.read()
 	json = json.load(f)
 	df = pd.DataFrame.from_dict(json)
-	randomized = df.sample(frac=1)
+	randomized = df.sample(frac=1).reset_index(drop=True)
+	pd.DataFrame.to_json("shuffled_basic.json")
+
+with open("shuffled_basic.json", 'r') as f:
+	data = f.read()
+	print data
