e36e432b2c4c1df1b80affca125a2a09bdb16d71
youdongz <youdongz@berkeley.edu>
2017-12-02 23:15:57 -0500

    pulling

--- analyze.py
+++ analyze.py
@@ -1,24 +1,6 @@
-import json
+import pandas as pd
 
-BRONZE = "BRONZE"
-SILVER = 'SILVER'
-GOLD = 'GOLD'
-PLATINUM = 'PLATINUM'
 
-classes = {}
-with open('decisions_10000.json', 'r') as f:
-	data = json.load(f)
-	for doc in data:
-		total = sum([1 if BRONZE in doc else 0, 1 if SILVER in doc else 0, 1 if GOLD in doc else 0, 1 if PLATINUM in doc else 0])
-		key = ((doc[BRONZE] if BRONZE in doc else 0) + doc[SILVER] + doc[GOLD] + doc[PLATINUM])/total
-		print doc, key
-		if key not in classes:
-			classes[key] = 0
-
-		classes[key] += 1
-
-lst = [[key] + [classes[key]] for key in classes]
-sort = sorted(lst, key=lambda k: k[0])
-values = [each[1] for each in sort]
-print sort
-print values
+with open('basic_decision_combined.json', 'r') as f:
+	df = pd.DataFrame.from_records(f.read())
+	print df
