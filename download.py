b05c6f1332ce08b718b836004bdfc2a3ace9c6aa
Andrea Jackson <andreaj@mit.edu>
2017-12-03 03:28:05 -0500

    changed titled, downloaded new data

--- download.py
+++ download.py
@@ -3,35 +3,35 @@ import json
 
 UNIT = 1482000
 
-# url = 'https://v3v10.vitechinc.com/solr/v_participant/select?indent=on&q=*:*&wt=json&rows=%d' % UNIT
-# print("Downloading...")
-# response = urllib.request.urlopen(url)
-
-# with open('basic_%d.json' % UNIT, 'ab') as f:
-# 	data = response.read()
-# 	f.write(data)
-
-# print("Done.")
-
-url = 'https://v3v10.vitechinc.com/solr/v_participant_detail/select?indent=on&q=*:*&wt=json&rows=%d' % UNIT
+url = 'https://v3v10.vitechinc.com/solr/v_participant/select?indent=on&q=*:*&wt=json&rows=%d' % UNIT
 print("Downloading...")
 response = urllib.request.urlopen(url)
 
-with open('specific_%d.json' % UNIT, 'ab') as f:
+with open('basic_%d.json' % UNIT, 'ab') as f:
 	data = response.read()
-	print("Writing data...")
 	f.write(data)
 
 print("Done.")
 
-
-# url = 'https://v3v10.vitechinc.com/solr/v_quotes/select?indent=on&q=*:*&wt=json&rows=%d' % UNIT
+# url = 'https://v3v10.vitechinc.com/solr/v_participant_detail/select?indent=on&q=*:*&wt=json&rows=%d' % UNIT
 # print("Downloading...")
 # response = urllib.request.urlopen(url)
 
-# with open('decision_%d.json' % UNIT, 'ab') as f:
+# with open('specific_%d.json' % UNIT, 'ab') as f:
 # 	data = response.read()
 # 	print("Writing data...")
 # 	f.write(data)
+
+# print("Done.")
+
+
+url = 'https://v3v10.vitechinc.com/solr/v_quotes/select?indent=on&q=*:*&wt=json&rows=%d' % UNIT
+print("Downloading...")
+response = urllib.request.urlopen(url)
+
+with open('decision_%d.json' % UNIT, 'ab') as f:
+	data = response.read()
+	print("Writing data...")
+	f.write(data)
 
 print("Done.")
