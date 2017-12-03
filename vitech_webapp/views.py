173b24428c425d0edf5acce6fa23a81f6da5a32b
youdongz <youdongz@berkeley.edu>
2017-12-03 06:00:09 -0500

    insurance page visuals

--- vitech_webapp/views.py
+++ vitech_webapp/views.py
@@ -1,5 +1,6 @@
 # import flask modules
-from flask import render_template, request, redirect, url_for, Flask
+from flask import render_template, request, redirect, url_for, Flask, flash
+from wtforms import Form, TextField, IntegerField, validators, SelectField, SubmitField
 
 # import jinja2 environment
 import jinja2
@@ -9,6 +10,18 @@ from flask_bootstrap import Bootstrap
 
 bootstrap = Bootstrap()
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
+    smoke = SelectField('Active Smoker (3+ / week)', choices=[('Yes', 'Yes'), ('no', 'No')], validators=[validators.required()])
+    number_covered = IntegerField('Number of Family Members Covered:', validators=[validators.required()])
+    insured_amount = IntegerField('Optional Insured Amount', default=500000, validators=[validators.required()])
+
+    def reset(self):
+        blankData = MultiDict([ ('csrf', self.reset_csrf() ) ])
+        self.process(blankData)
 
 # initialize extensions
 bootstrap.init_app(app)
@@ -19,15 +32,27 @@ def index():
 
 @app.route('/insurance', methods=['POST', 'GET'])
 def insurance():
-    return render_template('insurance.html')
+    form = ReusableForm(request.form)
 
-bar_graph_data = [1, 189, 206, 105, 121, 99, 145, 114, 119,
-					169, 194, 5, 71, 4, 2, 93, 2, 43, 1, 73,
-					27, 3, 40, 3, 4, 1, 4, 59, 66, 6, 1, 13,
-					2, 20, 1]
+    print form.errors
+    if request.method == 'POST':
+        name = request.form['name']
+        smoke = request.form['smoke']
+        number_covered = request.form['number_covered']
+        insured_amount = request.form['insured_amount']
+        
+        print name + smoke + number_covered + insured_amount
+ 
+        if form.validate():
+            return render_template('insurance.html', form=form, estimate=[0,1,2,3])
+        else:
+            flash('Please fill in all form fields.')
+ 
+    return render_template('insurance.html', form=form, estimate=[])
+
 @app.route('/trends', methods=['POST', 'GET'])
 def trends():
-    return render_template('trends.html', bar_graph=bar_graph_data)
+    return render_template('trends.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
